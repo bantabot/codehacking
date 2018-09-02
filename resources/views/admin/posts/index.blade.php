13d1b9565937af51ef607c8b1295221277bbce11
ali <alysebanta@gmail.com>
2018-09-02 17:09:07 -0400

    my first commit - admin views create

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: abanta
+ * Date: 9/2/18
+ * Time: 4:59 PM
+ */
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: abanta
+ * Date: 9/2/18
+ * Time: 5:01 PM
+ */
